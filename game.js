650255697de64a95a3e5fbf20b99547113d867c5
shubham9411 <shubham9411@gmail.com>
2017-02-25 12:02:53 +0530

    Initial commit

---
+++ game.js
@@ -0,0 +1,15 @@
+function evaluate( board){
+	console.log(board);
+}
+board = {
+	1:'o',
+	2:'_',
+	3:'o',
+	4:'_',
+	5:'x',
+	6:'o',
+	7:'x',
+	8:'o',
+	9:'x'
+};
+window.onload = evaluate( board );
